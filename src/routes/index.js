9c84adfd32641a60dd1c2375c0283a123cc6e2d6
Vlad <dohotaruv@gmail.com>
2017-04-08 17:54:51 +0300

    cateva pagini html previzorii pentru testare

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,13 @@ router.get('/admin/users/', adminRoutes.getAllUsers);
 router.get('/admin', (req, res) => {
   res.send("Admin access aproved");
 });
+
+router.get('/signup', (req, res) =>
+  res.render('signup.ejs'));
+
+router.get('/login', (req, res) =>
+  res.render('login.ejs'));
+
 router.get('/user', (req, res) => {
   res.send("User access aproved");
 });
